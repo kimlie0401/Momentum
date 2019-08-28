99acee460de9f2ac6c5d3a498910c04e7f5a7635
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-08-28 14:10:24 -0700

    Final

--- weather.js
+++ weather.js
@@ -1,5 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = "coords";
 const API_KEY = "f849cf6fae84a7be9d063a2c1643b903";
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=imperial`
+        ).then(
+            function(response){
+                return response.json();
+            }).then(
+                function(json){
+                    const temperture = json.main.temp;
+                    const place = json.name;
+                    weather.innerText = `${temperture} @ ${place}`;
+                });
+    
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,7 +29,7 @@ function handelGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
-
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -25,10 +41,12 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedcords = localStorage.getItem(COORDS);
-    if (loadedcords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
         askForCoords();
     }else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
         //get weather
     }
 }
